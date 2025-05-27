df661ad7e7c947d4fd9ddf214c2eca12805e1f10
loryy24 <lorychatigre@gmail.com>
2025-05-27 23:36:40 +0100

    log et reg

--- todo-list/routes/web.php
+++ todo-list/routes/web.php
@@ -7,11 +7,11 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\AuthController;
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 // Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
